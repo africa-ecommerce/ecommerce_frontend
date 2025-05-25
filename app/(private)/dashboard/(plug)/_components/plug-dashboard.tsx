7ef42e95e0a51cabc96ab9d6de5d704487f45484
kelpejol <paulkayode23@gmail.com>
2025-05-25 17:40:14 +0100

    commit

--- app/(private)/dashboard/(plug)/_components/plug-dashboard.tsx
+++ app/(private)/dashboard/(plug)/_components/plug-dashboard.tsx
@@ -897,8 +897,8 @@ export default function PlugDashboard() {
                       <Image 
                         src={platform.imageSrc}
                         alt={`${platform.platform} logo`}
-                        width={14}
-                        height={14}
+                        width={16}
+                        height={16}
                         className="object-contain"
                       />
                     </div>
