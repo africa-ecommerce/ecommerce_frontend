361558209df12a03a0519443cbd81895a72f45ca
kelpejol <paulkayode23@gmail.com>
2025-06-12 08:18:24 +0100

    pushed

--- app/onboarding/_components/supplierInfo.tsx
+++ app/onboarding/_components/supplierInfo.tsx
@@ -65,7 +65,6 @@ interface SupplierInfoProps {
   update: (p: FormData) => void;
   initialData?: {
     businessType?: BusinessType;
-    // pickupLocation?: string;
     businessName?: string;
     avatar?: File;
   };
--- zod/schema.ts
+++ zod/schema.ts
@@ -95,7 +95,8 @@ export const plugInfoSchema = z.object({
 
 
 export const profileSchema = z.object({
- 
+
+  professionalPhoto:  z.instanceof(File, {message: "Please provide your professional photo"}).describe("The professional photo file"), 
   businessName: z.string().min(2, {
     message: "Please provide your business name",
   }),
