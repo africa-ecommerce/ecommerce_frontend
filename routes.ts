5c4e031833c39e32c65e0103b37f53b751c5e619
kelpejol <paulkayode23@gmail.com>
2025-06-13 06:24:14 +0100

    pushed

--- routes.ts
+++ routes.ts
@@ -1,7 +1,7 @@
 
 
 /**@type {string[]}*/
-export const publicRoutes = ["/", "/product", "/checkout", "/order-error", "/thank-you"];
+export const publicRoutes = ["/", "/product", "/checkout", "/order-error", "/thank-you", "/subdomain-error", "/help"];
 
 /**@type {string[]}*/
 export const authRoutes = [
