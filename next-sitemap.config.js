15c15e2181bc9de1832ffd633c4be295a600ddd7
kelpejol <paulkayode23@gmail.com>
2025-07-13 02:32:00 +0100

    pushed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,40 @@
+// module.exports = {
+//   siteUrl: 'https://www.pluggn.com.ng',
+//   generateRobotsTxt: true,
+//   sitemapSize: 5000,
+//   transform: async (config, path) => {
+//     return {
+//       loc: path,
+//       changefreq: 'daily',
+//       priority: path === '/' ? 1.0 : 0.7,  // Set homepage priority higher
+//       lastmod: new Date().toISOString(),
+//     };
+//   },
+// };
+
+
+
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.pluggn.com.ng',
-  generateRobotsTxt: true,
+  siteUrl: 'https://www.pluggn.com.ng', // This tells the generator what your base URL is
+  generateRobotsTxt: true, // This enables automatic robots.txt generation
   sitemapSize: 5000,
+  exclude: ['/auth/*', '/terms', '/privacy',  "/onboarding", "/checkout", "/product", "/order-error", "/thank-you", "/subdomain-error", "/track-order"], // Exclude unwanted pages
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/auth/', '/terms', '/privacy', "/onboarding", "/checkout", "/product", "/order-error", "/thank-you", "/subdomain-error", "/track-order"],
+        allow: '/', // Allow everything else
+      },
+    ],
+   
+  },
   transform: async (config, path) => {
     return {
       loc: path,
       changefreq: 'daily',
-      priority: path === '/' ? 1.0 : 0.7,  // Set homepage priority higher
+      priority: path === '/' ? 1.0 : 0.7,
       lastmod: new Date().toISOString(),
     };
   },
