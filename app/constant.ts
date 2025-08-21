201490d50e6a6876024dd1413851e185eb5af7e0
kelpejol <paulkayode23@gmail.com>
2025-08-21 10:57:49 +0100

    done

--- app/constant.ts
+++ app/constant.ts
@@ -1,19 +1,39 @@
 export const PRODUCT_CATEGORIES = [
     { value: "all", label: "All Categories" },
-   { value: "footwears", label: "Footwears" },
+    { value: "footwears", label: "Footwears" },
   { value: "bags", label: "Bags" },
   { value: "accessories", label: "Accessories" },
   { value: "curated_fashion", label: "Curated Fashion" },
   { value: "home_decor", label: "Home Decor" },
+  { value: "beauty&_fragrance", label: "Beauty & Fragrance" },
+  { value: "fitness&_sports", label: "Fitness & Sports" },
+
+  { value: "books", label: "Books" },
+  { value: "toys&_games", label: "Toys & Games" },
+
+  { value: "pet_supplies", label: "Pet Supplies" },
+  { value: "art&_crafts", label: "Art & Crafts" },
+
+  { value: "baby_products", label: "Baby Products" },
 ];
 
 
 export const NICHE_CATEGORIES = [
-  { value: "footwears", label: "Footwears" },
+    { value: "footwears", label: "Footwears" },
   { value: "bags", label: "Bags" },
   { value: "accessories", label: "Accessories" },
   { value: "curated_fashion", label: "Curated Fashion" },
   { value: "home_decor", label: "Home Decor" },
+  { value: "beauty&_fragrance", label: "Beauty & Fragrance" },
+  { value: "fitness&_sports", label: "Fitness & Sports" },
+
+  { value: "books", label: "Books" },
+  { value: "toys&_games", label: "Toys & Games" },
+
+  { value: "pet_supplies", label: "Pet Supplies" },
+  { value: "art&_crafts", label: "Art & Crafts" },
+
+  { value: "baby_products", label: "Baby Products" },
 ];
 
 
