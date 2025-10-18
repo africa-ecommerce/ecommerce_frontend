22ebe8c88f03c7441c268a5a4aeab7f6145f51e0
kelpejol <paulkayode23@gmail.com>
2025-10-18 23:34:19 +0300

    done

--- hooks/use-discoverSync.ts
+++ hooks/use-discoverSync.ts
@@ -545,8 +545,8 @@ export function useDiscoverSync() {
       if (pendingExists) await processQueuedSyncs();
 
       if (changed && hasChanges) {
-        const acceptedChunks = chunkArray(accepted, 10);
-        const rejectedChunks = chunkArray(rejected, 10);
+        const acceptedChunks = chunkArray(accepted, 5);
+        const rejectedChunks = chunkArray(rejected, 5);
 
         console.log(
           `📦 Sending ${acceptedChunks.length} accepted batches and ${rejectedChunks.length} rejected batches`
