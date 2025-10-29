346cf87dad013e0e5dddd6489723fedc18e58f25
kelpejol <paulkayode23@gmail.com>
2025-10-29 06:25:08 +0300

    done

--- app/(private)/dashboard/(supplier)/channel/components/channel-header.tsx
+++ app/(private)/dashboard/(supplier)/channel/components/channel-header.tsx
@@ -1,21 +1,26 @@
 "use client";
 
+import { useUser } from "@/app/_components/provider/UserContext";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Store } from "lucide-react";
 
 export default function ChannelHeader() {
+
+  const {
+    userData: { user },
+  } = useUser();
   return (
     <header className="fixed top-0 left-0 right-0 bg-white border-b border-neutral-200 shadow-sm z-50 animate-in slide-in-from-top duration-500">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center gap-3">
         <Avatar className="h-10 w-10 border-2 border-orange-500">
-          <AvatarImage src="/supplier-logo.jpg" alt="Supplier" />
+          <AvatarImage src={`${user?.supplier?.avatar}`} alt="Supplier" />
           <AvatarFallback className="bg-orange-100 text-orange-600">
-            <Store className="h-5 w-5" />
+            {user?.supplier?.businessName?.slice(0, 2).toUpperCase()}
           </AvatarFallback>
         </Avatar>
         <div>
-          <h2 className="font-semibold text-neutral-800">
-            My Supplier Channel
+          <h2 className="font-semibold text-neutral-800 capitalize">
+           {user?.supplier?.businessName}
           </h2>
           <p className="text-xs text-neutral-500">Channel</p>
         </div>
